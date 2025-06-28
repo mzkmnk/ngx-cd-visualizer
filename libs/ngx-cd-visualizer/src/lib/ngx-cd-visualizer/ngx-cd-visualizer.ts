685f68f0819483f8e30c5eca2269b6de2fd52427
mzkmnk <sma0729mex@gmail.com>
2025-06-28 23:36:34 +0900

    fix: eslint error

---
+++ libs/ngx-cd-visualizer/src/lib/ngx-cd-visualizer/ngx-cd-visualizer.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ngx-cd-visualizer',
+  imports: [CommonModule],
+  templateUrl: './ngx-cd-visualizer.html',
+  styleUrl: './ngx-cd-visualizer.css',
+})
+export class NgxCdVisualizer { }
